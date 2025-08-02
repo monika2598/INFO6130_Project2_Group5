2a0b4c0458675091d92eb77e5b07f76fe4e8ac7c
Gouravdeep Singh <gouravdhaliwal88@gmail.com>
2025-08-02 16:38:54 -0400

    Commit 1 Gouravdeep

--- BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/ChangeTextBehaviorKtTest.kt
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/ChangeTextBehaviorKtTest.kt
@@ -1,84 +1,52 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.testing.espresso.BasicSample
 
-import androidx.test.ext.junit.rules.activityScenarioRule
-import android.app.Activity
-import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import android.view.View
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import org.junit.Before
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
-/**
- * The kotlin equivalent to the ChangeTextBehaviorTest, that
- * showcases simple view matchers and actions like [ViewMatchers.withId],
- * [ViewActions.click] and [ViewActions.typeText], and ActivityScenarioRule
- *
- *
- * Note that there is no need to tell Espresso that a view is in a different [Activity].
- */
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class ChangeTextBehaviorKtTest {
 
-    /**
-     * Use [ActivityScenarioRule] to create and launch the activity under test before each test,
-     * and close it after each test. This is a replacement for
-     * [androidx.test.rule.ActivityTestRule].
-     */
-    @get:Rule var activityScenarioRule = activityScenarioRule<MainActivity>()
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
+
 
     @Test
-    fun changeText_sameActivity() {
-
-        // Type text and then press the button.
-        onView(withId(R.id.editTextUserInput))
-                .perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard())
+    fun testChangeTextWith123() {
+        onView(withId(R.id.editTextUserInput)).perform(typeText("123"), closeSoftKeyboard())
         onView(withId(R.id.changeTextBt)).perform(click())
-
-        // Check that the text was changed.
-        onView(withId(R.id.textToBeChanged)).check(matches(withText(STRING_TO_BE_TYPED)))
+        onView(withId(R.id.textToBeChanged)).check(matches(withText("123")))
     }
 
     @Test
-    fun changeText_newActivity() {
-        // Type text and then press the button.
-        onView(withId(R.id.editTextUserInput)).perform(typeText(STRING_TO_BE_TYPED),
-                closeSoftKeyboard())
+    fun testOpenActivityWith123() {
+        onView(withId(R.id.editTextUserInput)).perform(typeText("123"), closeSoftKeyboard())
         onView(withId(R.id.activityChangeTextBtn)).perform(click())
-
-        // This view is in a different Activity, no need to tell Espresso.
-        onView(withId(R.id.show_text_view)).check(matches(withText(STRING_TO_BE_TYPED)))
+        onView(isRoot()).perform(waitFor(500)) // Wait for activity transition
+        onView(withId(R.id.show_text_view)).check(matches(withText("123")))
     }
 
-    companion object {
 
-        val STRING_TO_BE_TYPED = "Espresso"
+    // Optional: replaces Thread.sleep
+    private fun waitFor(delay: Long): ViewAction {
+        return object : ViewAction {
+            override fun getConstraints() = isRoot()
+            override fun getDescription() = "Wait for $delay milliseconds"
+            override fun perform(uiController: UiController, view: View?) {
+                uiController.loopMainThreadForAtLeast(delay)
+            }
+        }
     }
 }
